11416f9115126db165dc98b053f0f7c10ed90347
haru0801 <maru0819@docomo.ne.jp>
2023-12-18 21:06:14 +0900

    fillable

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,6 +9,11 @@ class Post extends Model
 {
     use HasFactory;
     
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+    
     public function getPaginateByLimit(int $limit_count = 10)
     {
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);

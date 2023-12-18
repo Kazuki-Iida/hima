cdb27be7a747ab6471270499795a3e9ee146807f
Kazuki-Iida <kazuki083177@gmail.com>
2023-12-18 21:47:56 +0900

    ポストコントローラー編集

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,8 +15,9 @@ class PostController extends Controller
     
     public function show(Post $post)
     {
-        return view('posts.show')->with(['posts' => $post]);
+        return view('posts.show')->with(['post' => $post]);
     }
+    
     public function create()
     {
         return view('posts/create');

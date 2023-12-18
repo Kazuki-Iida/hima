a08b6ea07b6650f4ef1bba58018511c274269bc5
Kazuki-Iida <kazuki083177@gmail.com>
2023-12-18 21:41:58 +0900

    ルーティング編集

--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,15 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/', [PostController::class, 'index'])->name('post.index');
-Route::get('/posts/{post}', [PostController::class, 'show'])->name('post.show');
-Route::get('/posts', [PostController::class, 'create'])->name('post.create');
-Route::post('/posts', [PostController::class, 'store'])->name('post.store');
-Route::get('/posts/{post}', [PostController::class, 'edit'])->name('post.edit');
-Route::put('/posts', [PostController::class, 'update'])->name('post.update');
-Route::delete('/posts', [PostController::class, 'delete'])->name('post.delete');
+Route::middleware('posts')->group(function () {
+    Route::get('/', [PostController::class, 'index'])->name('post.index');
+    Route::get('/posts/create', [PostController::class, 'create'])->name('post.create');
+    Route::get('/posts/{post}', [PostController::class, 'show'])->name('post.show');
+    Route::post('/posts', [PostController::class, 'store'])->name('post.store');
+    Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('post.edit');
+    Route::put('/posts', [PostController::class, 'update'])->name('post.update');
+    Route::delete('/posts', [PostController::class, 'delete'])->name('post.delete');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');

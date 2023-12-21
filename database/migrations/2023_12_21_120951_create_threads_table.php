feeb60c8a517e775ad947c0b9aa7c1694991c981
Kazuki-Iida <kazuki083177@gmail.com>
2023-12-21 21:21:50 +0900

    thread色々

---
+++ database/migrations/2023_12_21_120951_create_threads_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('threads', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50);
+            $table->string('body', 200);
+            $table->foreignId('post_id')->constrained();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('threads');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::controller(PostController::class)->group(function () {
     Route::get('/', 'index')->name('posts.index');
